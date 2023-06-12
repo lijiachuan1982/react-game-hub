6922eef2f462bccb75bf2d015de8c1f9228de841
lijiachuan1982 <jiachuan.li@outlook.com>
2023-06-12 16:07:06 +0800

    Sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return '';
   const index = url.indexOf("media/") + 6
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
 }
